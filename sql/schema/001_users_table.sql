a46c3943206ed58018dc36e057991e3820701dc9
ItzTas <talessabini2@gmail.com>
2024-06-13 22:44:05 -0300

    users table

---
+++ sql/schema/001_users_table.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    user_name TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    currency NUMERIC NOT NULL
+);
+
+-- +goose Down
+DROP TABLE users;
